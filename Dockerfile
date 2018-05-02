07678051752e2c6e4f54d70f4bafdc606d493861
dxk19911212 <648638730@qq.com>
2018-05-02 18:36:29 +0800

    java日志时区调整

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE
 COPY ${JAR_FILE} app.jar
-ENV JAVA_OPTS="\$JAVA_OPTS -Duser.timezone=GMT+08"
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
 EXPOSE 8881
--- src/main/java/com/iishoni/consulprovider/Application.java
+++ src/main/java/com/iishoni/consulprovider/Application.java
@@ -9,9 +9,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ImportResource;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringCloudApplication
 @EnableHystrix
 @EnableFeignClients
@@ -19,11 +16,6 @@ import java.util.TimeZone;
 @MapperScan("com.iishoni.consulprovider.mapper")
 @ImportResource("classpath*:spring-context.xml")
 public class Application {
-
-    @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
